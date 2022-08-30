2d4c678df5945a770ccae7eeae66680351792d8c
Ryan <superryanman1@gmail.com>
2022-08-30 16:19:49 -0700

    finished challenge 32? cant test currently

--- python/tests/code_challenges/test_tree_intersection.py
+++ python/tests/code_challenges/test_tree_intersection.py
@@ -1,18 +1,18 @@
-#import pytest
-#from codefellows.dsa.binary_tree import BinaryTree
-#from code_challenges.tree_intersection import tree_intersection#
+import pytest
+from codefellows.dsa.binary_tree import BinaryTree
+from code_challenges.tree_intersection import tree_intersection#
 
 #@pytest.mark.skip("TODO")
-#def test_exists():
-#    assert tree_intersection
-#    pass
+def test_exists():
+    assert tree_intersection
+    pass
 
 #@pytest.mark.skip("TODO")
-#def test_tree_intersection():
-#    tree_a = BinaryTree(values=[150, 100, 250, 75, 160, 200, 350, 125, 175, 300, 500])
-#    tree_b = BinaryTree(values=[42, 100, 100, 15, 160, 200, 350, 125, 175, 4, 500])
+def test_tree_intersection():
+    tree_a = BinaryTree(values=[150, 100, 250, 75, 160, 200, 350, 125, 175, 300, 500])
+    tree_b = BinaryTree(values=[42, 100, 100, 15, 160, 200, 350, 125, 175, 4, 500])
 
- #   actual = tree_intersection(tree_a, tree_b)
- #   expected = [125, 175, 100, 160, 500, 200, 350]
+    actual = tree_intersection(tree_a, tree_b)
+    expected = [125, 175, 100, 160, 500, 200, 350]
 
-  #  assert sorted(actual) == sorted(expected)
+    assert sorted(actual) == sorted(expected)
