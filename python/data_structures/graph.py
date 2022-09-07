3141e4ef3096240e1e9969f99197d9e760273105
Ryan <superryanman1@gmail.com>
2022-09-06 17:52:18 -0700

    finished challenge, will add whiteboard later

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -1,3 +1,5 @@
+from data_structures.queue import Queue
+
 class Graph:
     def __init__(self):
         # initialization here
@@ -12,7 +14,7 @@ class Graph:
         return len(self._adjacency_list)
 
     def get_nodes(self):
-        return self._adjacency_list.keys()
+        return list(self._adjacency_list.keys())
 
     def add_edge(self, start_vertex, end_vertex, weight=0):
         if start_vertex not in self._adjacency_list or end_vertex not in self._adjacency_list:
@@ -22,6 +24,23 @@ class Graph:
 
     def get_neighbors(self, vertex):
         return self._adjacency_list[vertex]
+
+    def breadth_first(self, vertex):
+        all_vertices = []
+        breadth = Queue()
+        visited_vertices = set()
+        breadth.enqueue(vertex)
+        visited_vertices.add(vertex)
+
+        while not breadth.is_empty():
+            front = breadth.dequeue()
+            all_vertices.append(front.value)
+
+            for neighbor in self.get_neighbors(front):
+                if neighbor.vertex not in visited_vertices:
+                    visited_vertices.add(neighbor.vertex)
+                    breadth.enqueue(neighbor.vertex)
+        return all_vertices
 
 class Vertex:
     def __init__(self, value):
