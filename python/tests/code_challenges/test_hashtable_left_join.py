61a13d64fa4c35d6f3e79f749cbe244d78c23e00
Ryan <superryanman1@gmail.com>
2022-09-14 22:28:50 -0700

    added new test features

--- python/tests/code_challenges/test_hashtable_left_join.py
+++ python/tests/code_challenges/test_hashtable_left_join.py
@@ -35,4 +35,60 @@ def test_example():
 
     assert actual == expected
 
+# @pytest.mark.skip("TODO")
+def test_custom_one():
+    synonyms = {
+        "meditative": "cognative",
+        "motorcade": "caravan",
+        "concurrent": "synchronous",
+        "surround": "encircle",
+        "agreeable": "copacetic",
+    }
+    antonyms = {
+        "meditative": "unreflective",
+        "agreeable": "disagreeable",
+        "destroy": "construct",
+        "apex": "nadir",
+        "declination": "ascent",
+    }
 
+    expected = [
+        ['meditative', 'cognative', 'unreflective'],
+        ['motorcade', 'caravan', 'NONE'],
+        ['concurrent', 'synchronous', 'NONE'],
+        ['surround', 'encircle', 'NONE'],
+        ['agreeable', 'copacetic', 'disagreeable']
+        ]
+
+    actual = left_join(synonyms, antonyms)
+
+    assert actual == expected
+
+# @pytest.mark.skip("TODO")
+def test_custom_two():
+    left = {
+        "1": "2",
+        "3": "4",
+        "5": "6",
+        "7": "8",
+        "9": "10",
+    }
+    right = {
+        "1": "word1",
+        "7": "word2",
+        "9": "word3",
+        "15": "word4",
+        "21": "word5",
+    }
+
+    expected = [
+    ['1', '2', 'word1'],
+    ['3', '4', 'NONE'],
+    ['5', '6', 'NONE'],
+    ['7', '8', 'word2'],
+    ['9', '10', 'word3']
+    ]
+
+    actual = left_join(left, right)
+
+    assert actual == expected
